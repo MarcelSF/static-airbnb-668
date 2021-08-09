b2cc571bb487085b04409efb039663b6f58024a9
Marcel Fonseca <marcelsf23br@gmail.com>
2021-08-09 17:27:30 +0200

    done show page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,6 +7,13 @@ class FlatsController < ApplicationController
     # Parse the JSON into Ruby
     @flats = JSON.parse(open(url).read)
     # Iterate over the contect of the JSON and organize and display that content
+  end
 
+  def show
+    # The show user action wants to display all the details of ONE Flat
+    # Get the info from the JSON
+    url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
+    flats = JSON.parse(open(url).read)
+    @flat = flats.find { |flat| flat["id"] == params[:id].to_i }
   end
 end
