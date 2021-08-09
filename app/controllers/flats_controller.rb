91d3f64a7117925f156e925954eab8b1fb97080a
Marcel Fonseca <marcelsf23br@gmail.com>
2021-08-09 17:05:40 +0200

    add second test

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,12 @@
+require 'open-uri'
+
 class FlatsController < ApplicationController
   def index
+    url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
+    # Access the URL with Open-URI
+    # Parse the JSON into Ruby
+    @flats = JSON.parse(open(url).read)
+    # Iterate over the contect of the JSON and organize and display that content
+
   end
 end
--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -6,4 +6,10 @@ class FlatsTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Take a look at out flats!"
   end
+
+  test 'visiting the SHOW page' do
+    visit 'flats/145'
+
+    assert_selector 'h1', text: 'Charm at the Steps of the Sacre Coeur/Montmartre'
+  end
 end
